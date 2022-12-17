bfb98976b7bd9d4572e52233a0a9fbfdb0899ff0
yangzhiqiang54 <yzq_goodjob@126.com>
2022-12-17 15:36:20 +0800

    add new file

---
+++ alaeve.h
@@ -0,0 +1,18 @@
+#ifndef _ALAEVE_H_
+#define _ALAEVE_H_
+
+#include "stdint.h"
+
+typedef struct ae_info_t {
+    uint16_t num; //±¨¾¯ÅÐ¶Ï×ÜÊý
+
+
+}ae_info_t;
+
+
+
+
+int ae_init(void);
+
+
+#endif
